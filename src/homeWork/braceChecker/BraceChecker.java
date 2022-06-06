7bf61c1159e9f1163ab6b99d54fbac4cc55795ce
Vl <valmail9779@gmail.com>
2022-06-06 21:58:46 +0400

    bchecker

--- src/homeWork/braceChecker/BraceChecker.java
+++ src/homeWork/braceChecker/BraceChecker.java
@@ -45,6 +45,51 @@ public class BraceChecker {
             }
         }
     }
+    public void checkOnlyOpen(){
+        for (int j = 0; j < text.length(); j++) {
+            switch (text.charAt(j)) {
+                case '(':
+                    char open1 = stack.pop();
+                    if (open1 != ')') {
+                        showError(text.charAt(j));
+                    }
+                    break;
+                case '{':
+                    char open2 = stack.pop();
+                    if (open2 != '}') {
+                        showError(text.charAt(j));
+                    }
+                    break;
+                case '[':
+                    char open3 = stack.pop();
+                    if (open3 != ']') {
+                        showError(text.charAt(j));
+                    }
+                    break;
+            }
+        }
+    }
+    public void checkOnlyClose(){
+        for (int k = 0; k < text.length(); k++) {
+            switch (stack.pop()){
+                case ')':
+                    if(text.charAt(k) != '('){
+                        showErrorMessage(stack.pop());
+                    }
+                    break;
+                case '}':
+                    if(text.charAt(k) != '{'){
+                        showErrorMessage(stack.pop());
+                    }
+                    break;
+                case ']':
+                    if(text.charAt(k) != '['){
+                        showErrorMessage(stack.pop());
+                    }
+                    break;
+            }
+        }
+    }
 
     public void showError(char opened, char closed) {
         System.out.print("Error: opened " + opened + " but closed " + closed + " ");
@@ -52,6 +97,9 @@ public class BraceChecker {
 
     public void showError(char opened) {
         System.out.println("Error: opened " + opened + " but not closed" + ". ");
+    }
+    public void showErrorMessage(char closed) {
+        System.out.println("Error: closed " + closed + " but not opened" + ". ");
     }
 }
 
--- src/homeWork/braceChecker/BraceCheckerTest.java
+++ src/homeWork/braceChecker/BraceCheckerTest.java
@@ -2,7 +2,9 @@ package homeWork.braceChecker;
 
 public class BraceCheckerTest {
     public static void main(String[] args) {
-        BraceChecker braceChecker = new BraceChecker("{]{)");
+        BraceChecker braceChecker = new BraceChecker("{()}]");
         braceChecker.check();
+        braceChecker.checkOnlyOpen();
+        braceChecker.checkOnlyClose();
     }
 }
