56dd1adc4c1706e0ecffb50e57eca7ac32b9ff48
Vl <valmail9779@gmail.com>
2022-06-06 23:03:58 +0400

    bchecker

--- src/homeWork/braceChecker/BraceChecker.java
+++ src/homeWork/braceChecker/BraceChecker.java
@@ -22,72 +22,37 @@ public class BraceChecker {
                     char pop = stack.pop();
                     if (pop != '(') {
                         showError(pop, text.charAt(i));
-                        System.out.print("at " + i);
+                        System.out.println("at " + i);
                     }
-                    System.out.println();
                     break;
                 case '}':
                     char pop1 = stack.pop();
                     if (pop1 != '{') {
                         showError(pop1, text.charAt(i));
-                        System.out.print("at " + i);
+                        System.out.println("at " + i);
                     }
-                    System.out.println();
                     break;
                 case ']':
                     char pop2 = stack.pop();
                     if (pop2 != '[') {
                         showError(pop2, text.charAt(i));
-                        System.out.print(" at " + i);
-                    }
-                    System.out.println();
-                    break;
-            }
-        }
-    }
-    public void checkOnlyOpen(){
-        for (int j = 0; j < text.length(); j++) {
-            switch (text.charAt(j)) {
-                case '(':
-                    char open1 = stack.pop();
-                    if (open1 != ')') {
-                        showError(text.charAt(j));
-                    }
-                    break;
-                case '{':
-                    char open2 = stack.pop();
-                    if (open2 != '}') {
-                        showError(text.charAt(j));
-                    }
-                    break;
-                case '[':
-                    char open3 = stack.pop();
-                    if (open3 != ']') {
-                        showError(text.charAt(j));
+                        System.out.println(" at " + i);
                     }
                     break;
             }
         }
     }
-    public void checkOnlyClose(){
-        for (int k = 0; k < text.length(); k++) {
-            switch (stack.pop()){
-                case ')':
-                    if(text.charAt(k) != '('){
-                        showErrorMessage(stack.pop());
-                    }
-                    break;
-                case '}':
-                    if(text.charAt(k) != '{'){
-                        showErrorMessage(stack.pop());
-                    }
-                    break;
-                case ']':
-                    if(text.charAt(k) != '['){
-                        showErrorMessage(stack.pop());
-                    }
-                    break;
-            }
+
+    public void checkOnlyOpen() {
+        for (int i = 0; i < stack.pop(); i++) {
+            showError(text.charAt(i));
+        }
+    }
+
+    public void checkOnlyClose() {
+        for (int k = 0; k < stack.pop(); k++) {
+            showErrorMessage(stack.pop());
+
         }
     }
 
@@ -98,6 +63,7 @@ public class BraceChecker {
     public void showError(char opened) {
         System.out.println("Error: opened " + opened + " but not closed" + ". ");
     }
+
     public void showErrorMessage(char closed) {
         System.out.println("Error: closed " + closed + " but not opened" + ". ");
     }
--- src/homeWork/braceChecker/BraceCheckerTest.java
+++ src/homeWork/braceChecker/BraceCheckerTest.java
@@ -2,9 +2,10 @@ package homeWork.braceChecker;
 
 public class BraceCheckerTest {
     public static void main(String[] args) {
-        BraceChecker braceChecker = new BraceChecker("{()}]");
+        BraceChecker braceChecker = new BraceChecker("{}{[()");
         braceChecker.check();
         braceChecker.checkOnlyOpen();
-        braceChecker.checkOnlyClose();
+        //braceChecker.checkOnlyClose();
+
     }
 }
