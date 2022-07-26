1f11c98ad13050d0b292ccdfec692f2260495079
user <valmail9779@gmail.com>
2022-07-26 14:47:02 +0400

    collection

--- src/homeWork/collection/MapExample.java
+++ src/homeWork/collection/MapExample.java
@@ -1,9 +1,6 @@
 package homeWork.collection;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MapExample {
 
@@ -12,11 +9,18 @@ public class MapExample {
         fbTime.add(new FootballTeamMember(7,"Poxos Poxosyan"));
         fbTime.add(new FootballTeamMember(10,"Martiros Martirosyan"));
         fbTime.add(new FootballTeamMember(1,"Poxos Petrosyan"));
-        fbTime.add(new FootballTeamMember(8,"Armen Vardanyan"));
-        fbTime.add(new FootballTeamMember(23,"Petros Poxosyan"));
+        fbTime.add(new FootballTeamMember(6,"Armen Vardanyan"));
+        fbTime.add(new FootballTeamMember(12,"Tigran Adamyan"));
+        fbTime.add(new FootballTeamMember(11,"Armen Arshakyan"));
+        fbTime.add(new FootballTeamMember(8,"Davit Poxosyan"));
+        fbTime.add(new FootballTeamMember(3,"Gagik Muradyan"));
+        fbTime.add(new FootballTeamMember(5,"Sergey Stepanyan"));
+        fbTime.add(new FootballTeamMember(10,"Vazgen Aramyan"));
+        fbTime.add(new FootballTeamMember(4,"Aram Kirakosyan"));
+        fbTime.add(new FootballTeamMember(9,"Petros Poxosyan"));
         //System.out.println(createFootballTeam(fbTime));
 
-        HashMap <Integer,String> members= new HashMap<>();
+        TreeMap <Integer,String> members= new TreeMap<>();
         for (FootballTeamMember fb : fbTime) {
             members.put(fb.getNumber(),fb.getName());
         }
